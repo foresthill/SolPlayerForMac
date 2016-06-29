19b5a8f9d398942d6a8a2b402959a06d111ad8ba
Morioka Naoya <g1989n@gmail.com>
2016-06-30 02:30:22 +0900

    Initial Commit

---
+++ SolPlayer for Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SolPlayer for Mac
+//
+//  Created by Morioka Naoya on H28/06/30.
+//  Copyright © 平成28年 Morioka Naoya. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
